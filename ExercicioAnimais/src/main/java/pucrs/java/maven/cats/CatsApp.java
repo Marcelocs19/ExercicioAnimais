c004b7401880f96cf6ec6816d5a1e5c2c6b70508
marcelocs19 <marcelo.home19@gmail.com>
2019-06-11 14:38:20 -0300

    #1

---
+++ ExercicioAnimais/src/main/java/pucrs/java/maven/cats/CatsApp.java
@@ -0,0 +1,16 @@
+package pucrs.java.maven.cats;
+
+/**
+ * 
+ * The class GatosApp is pet clinic system starting
+ *
+ */
+public class CatsApp {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+	}
+	
+}
---
+++ ExercicioAnimais/src/test/java/pucrs/java/maven/cats/CatsAppTest.java
@@ -0,0 +1,19 @@
+package pucrs.java.maven.cats;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CatsAppTest {
+
+
+	/**
+	 * 
+	 */
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
