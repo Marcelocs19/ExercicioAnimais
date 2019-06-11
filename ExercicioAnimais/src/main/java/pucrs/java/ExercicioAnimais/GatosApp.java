23822ce8ca75be897c32b36ece099dff9af7ab84
marcelocs19 <marcelo.home19@gmail.com>
2019-06-11 14:25:37 -0300

    #1

---
+++ ExercicioAnimais/src/main/java/pucrs/java/ExercicioAnimais/GatosApp.java
@@ -0,0 +1,10 @@
+package pucrs.java.ExercicioAnimais;
+
+
+public class GatosApp {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+}
---
+++ ExercicioAnimais/src/test/java/pucrs/java/ExercicioAnimais/GatosAppTest.java
@@ -0,0 +1,33 @@
+package pucrs.java.ExercicioAnimais;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class GatosAppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public GatosAppTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(GatosAppTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
+}
